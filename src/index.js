0f6f34491005f8556b38ee8df7ba779e3339705c
Rob <robert.gentile@powerhrg.com>
2021-07-30 10:43:43 -0400

    Complete

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { BrowserRouter } from "react-router-dom"
 import "./index.css"
 import App from "./App"
 
-import "playbook-ui/dist/fonts/fontawesome-min"
-import "playbook-ui/dist/fonts/regular-min"
-import "playbook-ui/dist/playbook.css"
+// import "playbook-ui/dist/fonts/fontawesome-min"
+// import "playbook-ui/dist/fonts/regular-min"
+// import "playbook-ui/dist/playbook.css"
 
 ReactDOM.render(
   <BrowserRouter>
